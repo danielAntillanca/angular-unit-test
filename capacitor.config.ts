ccb17c441966c4635ce0d472e86b10a6d5e304e1
daniel <da.antillancac@duocuc.cl>
2022-12-02 13:19:40 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular-unit-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
