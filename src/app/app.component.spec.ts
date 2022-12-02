ccd8b0638899e12c582ef8996c4befe660ff110b
daniel <da.antillancac@duocuc.cl>
2022-12-02 13:59:11 -0400

    unidad3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,80 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+//TODO: "spec.ts <--"
+//TODO: 😨 es la sintaxis de Jasmin!
+
+describe(`(1) TEST del componente "AppComponent"`, () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule//TODO: <-----
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
+
   });
 
-  it('should create the app', () => {
+  //TODO:Aislado! 
+  it('Debe de existir el AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy(); //TODO: ✔
   });
+
+  //TODO:Aislado! 
+  it('Debe retornar formulario invalido', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance
+    fixture.detectChanges() //TODO: <---------------
+
+    const email = app.form.controls['email']
+    email.setValue('unitaria@gmail.com')
+
+    expect(app.form.invalid).toBeTrue(); //TODO: ✔
+  });
+
+  it('Debe retornar formulario valido', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance
+    fixture.detectChanges()
+
+    let email = app.form.controls['email']
+    let password = app.form.controls['password']
+    let result = app.form.controls['result']
+
+    email.setValue('unitaria@gmail.com')
+    password.setValue('700')
+    result.setValue('1')
+
+
+    expect(app.form.invalid).toBeFalse(); //TODO: ✔
+  });
+
+  //TODO:Aislado!
+  // it(`Debe de actulizar datos de usuario`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   fixture.detectChanges()
+
+  //   let email = app.form.controls['email']
+  //   let password = app.form.controls['password']
+
+  //   email.setValue('prueba33@gmail.com')
+  //   password.setValue('123456')
+
+  //   const btnElement = fixture.debugElement.query(By.css('button.btn'))
+  //   btnElement.nativeElement.click()
+  //   const testData = { user: 1 }
+  //   expect(app.isCheck).toEqual(testData)
+  // });
 
 });
